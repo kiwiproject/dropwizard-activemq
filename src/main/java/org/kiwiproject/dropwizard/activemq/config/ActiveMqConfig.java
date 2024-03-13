6f3d641bcd0dde1dbd78b73d4826d691ce20b8a8
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-03-13 17:46:10 -0400

    Start adding configuration interfaces and classes (#6)

---
+++ src/main/java/org/kiwiproject/dropwizard/activemq/config/ActiveMqConfig.java
@@ -0,0 +1,11 @@
+package org.kiwiproject.dropwizard.activemq.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ActiveMqConfig {
+
+    // TODO
+}
---
+++ src/main/java/org/kiwiproject/dropwizard/activemq/config/ActiveMqHealthConfig.java
@@ -0,0 +1,43 @@
+package org.kiwiproject.dropwizard.activemq.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.dropwizard.util.Duration;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Configuration for ActiveMQ stats health checks that use Jolokia to get ActiveMQ stats.
+ */
+@Getter
+@Setter
+public class ActiveMqHealthConfig {
+
+    /**
+     * Username that Jolokia will use making HTTP requests using Basic Authentication.
+     */
+    @NotBlank
+    private String jmxUser;
+
+    /**
+     * Password that Jolokia will use making HTTP requests using Basic Authentication.
+     */
+    @NotBlank
+    private String jmxCred;
+
+    @NotNull
+    private List<String> ignoredDestinations = new ArrayList<>();
+
+    private int minConsumerThreshold;
+
+    private int maxPendingThreshold = 100;
+
+    @NotNull
+    private Duration refreshInterval = Duration.minutes(2);
+
+    private boolean ignoreEmptyQueuesWithNoConsumers = true;
+}
