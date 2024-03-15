70279b1b0ff0bd4b274c8f711a07235f28d97986
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-03-15 12:06:41 -0400

    Implement ActiveMqConsumer interface (#14)

--- src/main/java/org/kiwiproject/dropwizard/activemq/ActiveMqConsumer.java
+++ src/main/java/org/kiwiproject/dropwizard/activemq/ActiveMqConsumer.java
@@ -1,6 +1,90 @@
 package org.kiwiproject.dropwizard.activemq;
 
+import static org.kiwiproject.dropwizard.activemq.util.MessageTypeParser.UNKNOWN_MESSAGE_TYPE;
+
+import org.kiwiproject.dropwizard.activemq.exception.ActiveMqMessageException;
+import org.kiwiproject.dropwizard.activemq.exception.ActiveMqMessageInvalidMessageTypeException;
+import org.kiwiproject.dropwizard.activemq.exception.ActiveMqMessageMissingBodyException;
+import org.kiwiproject.dropwizard.activemq.util.MessageTypeParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface ActiveMqConsumer {
 
-    // TODO
+    enum Result {
+        CONSUMED, IGNORED
+    }
+
+    /**
+     * Consume a JMS message.
+     *
+     * @param message the {@link ActiveMqMessage} to consume
+     * @return {@link Result#CONSUMED} if the message was consumed, {@link Result#IGNORED} otherwise
+     */
+    Result consume(ActiveMqMessage message);
+
+    /**
+     * Validate that the {@link ActiveMqMessage} has a non-empty body
+     * or throw a {@link ActiveMqMessageMissingBodyException}.
+     *
+     * @param message the message to validate
+     * @return the body
+     * @throws ActiveMqMessageMissingBodyException if the body is {@code null} or blank
+     */
+    default String requireValidBody(ActiveMqMessage message) {
+        return message.getBody().orElseThrow(ActiveMqMessageMissingBodyException::new);
+    }
+
+    /**
+     * Valdiate that the {@link ActiveMqMessage} has a valid message type which is not
+     * equal to {@link MessageTypeParser#UNKNOWN_MESSAGE_TYPE}.
+     * <p>
+     * Only use this when a message is required to have a <strong>known</strong> message type.
+     *
+     * @param message the message to validate
+     * @return the message type
+     * @throws ActiveMqMessageInvalidMessageTypeException if the message type is missing, blank, or
+     * equals the UNKNOWN_MESSAGE_TYPE
+     */
+    default String requireValidMessageType(ActiveMqMessage message) {
+        var messageType = message.getMessageType()
+                .orElseThrow(() -> new ActiveMqMessageInvalidMessageTypeException("No message type present"));
+
+        if (UNKNOWN_MESSAGE_TYPE.equals(messageType)) {
+            throw new ActiveMqMessageInvalidMessageTypeException("Type parser was unable to identify the message type");
+        }
+
+        return messageType;
+    }
+
+    /**
+     * Handle any {@link ActiveMqMessageException} that may be generated be the ActiveMqConsumer.
+     * <p>
+     * This method is primarily exposed for testing purposes, but can also be used by implementations
+     * of {@link ActiveMqConsumer} for custom exception handling.
+     * <p>
+     * This default implementation logs a WARN-level message, and also a TRACE-level message containing
+     * the actual message and the stack trace. If many messages are failing, this allows you to change
+     * the log level to TRACE (temporarily) to view message contents for aid when debugging a problem.
+     *
+     * @param message the message
+     * @param e the exception to handle
+     */
+    default void handleException(ActiveMqMessage message, ActiveMqMessageException e) {
+        var logger = getLogger();
+
+        logger.warn("Failed to process an ActiveMqMessage: " +
+                "exception type={}, contentType={}, messageType={}, reason={} (body and stack trace at TRACE level)",
+                e.getCategory(),
+                message.getContentType(),
+                message.getMessageType(),
+                e.getMessage()
+        );
+
+        logger.trace("Message: {}", message.getBody(), e);
+    }
+
+    private static Logger getLogger() {
+        return LoggerFactory.getLogger(ActiveMqConsumer.class);
+    }
 }
