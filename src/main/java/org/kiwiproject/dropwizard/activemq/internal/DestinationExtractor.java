768e17189b5cafcf8557f7a39c9b14de4ec34ceb
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-03-20 16:09:49 +0000

    Finish ElucidationConfigurator

--- src/main/java/org/kiwiproject/dropwizard/activemq/internal/DestinationExtractor.java
+++ src/main/java/org/kiwiproject/dropwizard/activemq/internal/DestinationExtractor.java
@@ -3,6 +3,9 @@ package org.kiwiproject.dropwizard.activemq.internal;
 import lombok.experimental.UtilityClass;
 
 import org.apache.activemq.command.ActiveMQDestination;
+import org.kiwiproject.dropwizard.activemq.ActiveMqMessage;
+
+import java.util.regex.Pattern;
 
 @UtilityClass
 class DestinationExtractor {
@@ -10,6 +13,10 @@ class DestinationExtractor {
     // TODO
 
     private static final String DELIMITER = "::";
+
+    static final Pattern DELIMITER_SPLITTER = Pattern.compile(DELIMITER);
+
+    static final String BYTES_MESSAGE_TYPE = ActiveMqMessage.ContentType.BYTES.convertToMessageType();
 
     static String simplifyDestination(ActiveMQDestination originalDestination) {
         return simplifyDestination(originalDestination.toString());
