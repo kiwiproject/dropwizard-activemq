46d135ba1df3a177392b58d9bdcf9e4ab400e4a5
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-03-21 15:54:58 +0000

    Finish ProducerDelegate and test

--- src/main/java/org/kiwiproject/dropwizard/activemq/internal/ProducerDelegate.java
+++ src/main/java/org/kiwiproject/dropwizard/activemq/internal/ProducerDelegate.java
@@ -1,30 +1,35 @@
 package org.kiwiproject.dropwizard.activemq.internal;
 
+import static org.apache.commons.lang3.StringUtils.startsWith;
 import static org.kiwiproject.base.KiwiPreconditions.checkArgumentNotNull;
 import static org.kiwiproject.base.KiwiPreconditions.requireNotBlank;
 import static org.kiwiproject.base.KiwiPreconditions.requireNotNull;
 import static org.kiwiproject.base.KiwiStrings.f;
 import static org.kiwiproject.dropwizard.activemq.ActiveMqConstants.ALL_EVENTS_QUEUE;
+import static org.kiwiproject.dropwizard.activemq.ActiveMqProducer.PayloadDestination.SPECIFIED_AND_ALL_EVENTS;
+import static org.kiwiproject.dropwizard.activemq.internal.DestinationExtractor.createElucidationDestination;
+import static org.kiwiproject.dropwizard.activemq.internal.DestinationExtractor.simplifyDestinations;
+import static org.kiwiproject.dropwizard.activemq.util.DynamicDestinations.DYNAMIC_DESTINATION_ID;
 
 import com.google.common.annotations.VisibleForTesting;
 
 import lombok.extern.slf4j.Slf4j;
 
 import org.kiwiproject.dropwizard.activemq.ActiveMqProducer;
-import org.kiwiproject.dropwizard.activemq.util.DynamicDestinations;
 import org.kiwiproject.elucidation.client.ElucidationClient;
 
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.jms.ConnectionFactory;
 
 @Slf4j
 public class ProducerDelegate implements ActiveMqProducer {
 
-    private static final String PRODUCER_NOT_FOUND = "Producer '{}' not found";
+    private static final String PRODUCER_NOT_FOUND = "Producer '{}' not found!";
 
     @VisibleForTesting
     final Map<String, Producer> producers = new HashMap<>();
@@ -33,8 +38,6 @@ public class ProducerDelegate implements ActiveMqProducer {
     private final Duration timeToLive;
     private final ElucidationClient<String> elucidation;
     private final String serviceName;
-
-    // TODO...
 
     public ProducerDelegate(ConnectionFactory factory,
                             List<String> destinations,
@@ -63,7 +66,7 @@ public class ProducerDelegate implements ActiveMqProducer {
         }
 
         if (allowDynamicDestinations) {
-            putNewProducer(DynamicDestinations.DYNAMIC_DESTINATION_ID, factory);
+            putNewProducer(DYNAMIC_DESTINATION_ID, factory);
         }
 
         this.elucidation = requireNotNull(elucidation);
@@ -89,10 +92,17 @@ public class ProducerDelegate implements ActiveMqProducer {
     }
 
     /**
-     * Does this producer produce to the specified destination and is not a default producer?
+     * Does this producer produce to the specified destination, which must not be a default producer?
      */
     public boolean containsDestination(String destination) {
         return producers.containsKey(destination) && !producers.get(destination).isDefaultProducer();
+    }
+
+    /**
+     * Does this producer produce to the specified destination, which must be a default producer?
+     */
+    public boolean containsDefaultDestination(String destination) {
+        return producers.containsKey(destination) && producers.get(destination).isDefaultProducer();
     }
 
     @Override
@@ -111,26 +121,88 @@ public class ProducerDelegate implements ActiveMqProducer {
     }
 
     private boolean isDynamicDestination(String destination) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDynamicDestination'");
+        return allowDynamicDestinations && startsWith(destination, DYNAMIC_DESTINATION_ID + ":");
     }
 
-    private void produceToDynamicDestination(String destination, String payload, PayloadDestination payloadDestination,
-            Map<String, Object> headers) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'produceToDynamicDestination'");
+    private void produceToDynamicDestination(String destination,
+                                             String payload,
+                                             PayloadDestination payloadDestination,
+                                             Map<String, Object> headers) {
+
+        LOG.trace("Sending to dynamic destination: {} ; Payload: {}", destination, payload);
+
+        producers.get(DYNAMIC_DESTINATION_ID).produce(payload, destination, headers);
+
+        produceToAllEventsIfNeeded(destination, payload, payloadDestination, headers);
+
+        recordElucidationEvent(destination, payload);
     }
 
-    private void produceToKnownDestination(String destination, String payload, PayloadDestination payloadDestination,
-            Map<String, Object> headers) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'produceToKnownDestination'");
+    private void produceToKnownDestination(String destination,
+                                           String payload,
+                                           PayloadDestination payloadDestination,
+                                           Map<String, Object> headers) {
+
+        LOG.trace("Sending to destination: {} ; Payload: {}", destination, payload);
+
+        producers.get(destination).produce(payload, headers);
+
+        produceToAllEventsIfNeeded(destination, payload, payloadDestination, headers);
+
+        recordElucidationEvent(destination, payload);
+    }
+
+    private void produceToAllEventsIfNeeded(String destination,
+                                            String payload,
+                                            PayloadDestination payloadDestination,
+                                            Map<String, Object> headers) {
+
+        if (payloadDestination == SPECIFIED_AND_ALL_EVENTS) {
+            produceToDefaultDestinations(destination, payload, headers);
+        }
+    }
+
+    private void produceToDefaultDestinations(String excludedDestination,
+                                              String payload,
+                                              Map<String, Object> headers) {
+
+        producers.entrySet().stream()
+                .filter(destinationAndProducer ->
+                        destinationIsNot(excludedDestination, destinationAndProducer) &&
+                        isDefaultProducer(destinationAndProducer))
+                .forEach(destinationAndProducer -> {
+                    destinationAndProducer.getValue().produce(payload, headers);
+                    recordElucidationEvent(destinationAndProducer.getKey(), payload);
+                });
+    }
+
+    private static boolean destinationIsNot(String destination, Entry<String, Producer> destinationAndProducer) {
+        var key = destinationAndProducer.getKey();
+
+        return !key.equals(DYNAMIC_DESTINATION_ID) && !key.endsWith(destination);
+    }
+
+    private static boolean isDefaultProducer(Map.Entry<String, Producer> destinationAndProducer) {
+        return destinationAndProducer.getValue().isDefaultProducer();
     }
 
     @Override
     public void produceBytesMessage(String destination, byte[] payload) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'produceBytesMessage'");
+        LOG.trace("Sending bytes message to {}", destination);
+
+        if (producers.containsKey(destination)) {
+            producers.get(destination).produceBytesMessage(payload);
+
+            recordElucidationEvent(destination, DestinationExtractor.BYTES_MESSAGE_TYPE);
+        } else {
+            throwIllegalArgumentExceptionForUnknownDestination(destination);
+        }
+    }
+
+    private void recordElucidationEvent(String destination, String payload) {
+        simplifyDestinations(destination).stream()
+                .map(dest -> createElucidationDestination(dest, payload))
+                .forEach(event -> elucidation.recordNewEvent(event).whenComplete(ElucidationLogger::logResult));
     }
 
     private static void throwIllegalArgumentExceptionForUnknownDestination(String destination) {
---
+++ src/test/java/org/kiwiproject/dropwizard/activemq/internal/ProducerDelegateTest.java
@@ -0,0 +1,358 @@
+package org.kiwiproject.dropwizard.activemq.internal;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.kiwiproject.collect.KiwiMaps.newHashMap;
+import static org.kiwiproject.dropwizard.activemq.internal.ProducerDelegateTest.DestinationType.DYNAMIC;
+import static org.kiwiproject.dropwizard.activemq.internal.ProducerDelegateTest.DestinationType.NAMED;
+import static org.kiwiproject.dropwizard.activemq.test.util.TestObjectFactory.uniqueServiceName;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.kiwiproject.elucidation.client.ElucidationClient;
+import org.kiwiproject.elucidation.client.ElucidationResult;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+import javax.jms.ConnectionFactory;
+
+@DisplayName("ProducerDelegate")
+class ProducerDelegateTest {
+
+    enum DestinationType {
+        NAMED, DYNAMIC
+    }
+
+    private static final String NAMED_DESTINATION = "queue:test";
+    private static final String DYNAMIC_IDENTIFIER = "*";
+    private static final String DYNAMIC_DESTINATION = "*:test";
+    private static final String ALL_EVENTS_DESTINATION = "queue:all_events";
+
+    private Producer testProducer;
+    private Producer allEventsProducer;
+    private  ConnectionFactory factory;
+    private ElucidationClient<String> elucidationClient;
+    private String serviceName;
+
+    @BeforeEach
+    void setUp() {
+        testProducer = mock(Producer.class);
+        allEventsProducer = mock(Producer.class);
+
+        factory = mock(ConnectionFactory.class);
+
+        elucidationClient = mockElucidationClient();
+        var result = ElucidationResult.fromSkipMessage("Recorder not enabled");
+        when(elucidationClient.recordNewEvent(anyString())).thenReturn(CompletableFuture.completedFuture(result));
+
+        serviceName = uniqueServiceName();
+    }
+
+    @SuppressWarnings("unchecked")
+    private ElucidationClient<String> mockElucidationClient() {
+        return mock(ElucidationClient.class);
+    }
+
+    @Test
+    void shouldCreateNewInstance_WithNoDestinations() {
+        var delegate = newDefaultProducerDelegate(List.of(), List.of());
+
+        assertAll(
+            () -> assertThat(delegate.producers).containsOnlyKeys(ALL_EVENTS_DESTINATION),
+            () -> assertThat(delegate.containsDefaultDestination(ALL_EVENTS_DESTINATION)).isTrue()
+        );
+    }
+
+    @Test
+    void shouldCreateNewInstance_WithOneDestination_AndContainingAllEventsQueue() {
+        var delegate = newDefaultProducerDelegate(List.of(NAMED_DESTINATION), List.of());
+
+        assertAll(
+            () -> assertThat(delegate.producers).containsOnlyKeys(NAMED_DESTINATION, ALL_EVENTS_DESTINATION),
+            () -> assertThat(delegate.containsDestination(NAMED_DESTINATION)).isTrue(),
+            () -> assertThat(delegate.containsDefaultDestination(ALL_EVENTS_DESTINATION)).isTrue()
+        );
+    }
+
+    @Test
+    void shouldCreateNewInstance_WithAllEventsQueue_AsNormalDestination() {
+        var delegate = newDefaultProducerDelegate(List.of(ALL_EVENTS_DESTINATION), List.of());
+
+        assertAll(
+            () -> assertThat(delegate.producers).containsOnlyKeys(ALL_EVENTS_DESTINATION),
+            () -> assertThat(delegate.containsDestination(ALL_EVENTS_DESTINATION)).isTrue(),
+            () -> assertThat(delegate.containsDefaultDestination(ALL_EVENTS_DESTINATION)).isFalse()
+        );
+    }
+
+    @Test
+    void shouldCreateNewInstance_WithSingleDestination_AndMultipleDefaultDestinations() {
+        var defaultDestinations = List.of("queue1", "queue2");
+        var delegate = newDefaultProducerDelegate(List.of("test"), defaultDestinations);
+
+        assertAll(
+            () -> assertThat(delegate.producers).containsOnlyKeys("test", "queue1", "queue2"),
+            () -> assertThat(delegate.containsDestination("test")).isTrue(),
+            () -> assertThat(delegate.containsDefaultDestination("queue1")).isTrue(),
+            () -> assertThat(delegate.containsDefaultDestination("queue2")).isTrue(),
+            () -> assertThat(delegate.containsDefaultDestination(ALL_EVENTS_DESTINATION)).isFalse()
+        );
+    }
+
+    private ProducerDelegate newDefaultProducerDelegate(List<String> destinations, List<String> defaultDestinations) {
+        return new ProducerDelegate(factory,
+                destinations,
+                defaultDestinations,
+                false,
+                Duration.ZERO,
+                ElucidationClient.noop(),
+                serviceName);
+    }
+
+    @Test
+    void shouldProduceToAllEvents_whenUsingDynamicDestinations_AndNoExplicitDefaultDestinations() {
+        assertProducesToAllEventsAsDefaultDestination(DYNAMIC);
+    }
+
+    @Test
+    void shouldProduceToAllEvents_whenUsingNamedDestinations_AndNoExplicitDefaultDestinations() {
+        assertProducesToAllEventsAsDefaultDestination(NAMED);
+    }
+
+    private void assertProducesToAllEventsAsDefaultDestination(DestinationType destinationType) {
+        var payload = "Test!";
+
+        String destination;
+        ProducerDelegate delegate;
+
+        if (destinationType == DYNAMIC) {
+            destination = DYNAMIC_DESTINATION;
+            delegate = newDefaultProducerDelegateAllowingDynamicDestinations();
+            doNothing().when(testProducer).produce(payload, destination);
+        } else {
+            destination = NAMED_DESTINATION;
+            delegate = newDefaultProducerDelegateNoDynamicDestinations();
+            doNothing().when(testProducer).produce(payload);
+        }
+
+        when(allEventsProducer.isDefaultProducer()).thenReturn(true);
+        doNothing().when(allEventsProducer).produce(payload);
+
+        delegate.produceToDestinationAndAllEventsQueue(destination, payload);
+
+        if (destinationType == DYNAMIC) {
+            verify(testProducer).produce(payload, destination, Map.of());
+        } else {
+            verify(testProducer).produce(payload, Map.of());
+        }
+
+        verify(allEventsProducer).isDefaultProducer();
+        verify(allEventsProducer).produce(payload, Map.of());
+
+        verifyNoMoreInteractions(testProducer, allEventsProducer);
+
+        verifyElucidationWasInvokedProperly(destination, payload, ALL_EVENTS_DESTINATION);
+    }
+
+    private ProducerDelegate newDefaultProducerDelegateAllowingDynamicDestinations() {
+        return newDefaultProducerDelegateAllowingDynamicDestinations(
+                true,
+                DYNAMIC_IDENTIFIER, testProducer,
+                ALL_EVENTS_DESTINATION, allEventsProducer
+        );
+    }
+
+    private ProducerDelegate newDefaultProducerDelegateAllowingDynamicDestinations(boolean allowDynamicDestinations,
+                                                                                   Object... destinationToProducers) {
+        var delegate = new ProducerDelegate(factory,
+                List.of(),
+                List.of(),
+                allowDynamicDestinations,
+                Duration.ZERO,
+                elucidationClient,
+                serviceName);
+
+        delegate.producers.putAll(newHashMap(destinationToProducers));
+
+        return delegate;
+    }
+
+    @Test
+    void shouldProduceToExplicitDefaultDestinations_whenUsingDynamicDestinations_AndExplicitDefaultDestinations() {
+        assertProducesToExplicitDefaultDestinations(DYNAMIC);
+    }
+
+    @Test
+    void shouldProduceToExplicitDefaultDestinations_whenUsingNamedDestinations_AndExplicitDefaultDestinations() {
+        assertProducesToExplicitDefaultDestinations(NAMED);
+    }
+
+    private void assertProducesToExplicitDefaultDestinations(DestinationType destinationType) {
+        var payload = "Test!";
+
+        var queue1 = "queue:Q1";
+        var queue1Producer = newMockDefaultProducer(payload, queue1);
+
+        var queue2 = "queue:Q2";
+        var queue2Producer = newMockDefaultProducer(payload, queue2);
+
+        String identifier;
+        String destination;
+        ProducerDelegate delegate;
+        boolean allowDynamicDestinations;
+
+        if (destinationType == DYNAMIC) {
+            identifier = DYNAMIC_IDENTIFIER;
+            destination = DYNAMIC_DESTINATION;
+            allowDynamicDestinations = true;
+            doNothing().when(testProducer).produce(payload, destination, Map.of());
+        } else {
+            identifier = NAMED_DESTINATION;
+            destination = NAMED_DESTINATION;
+            allowDynamicDestinations = false;
+            doNothing().when(testProducer).produce(payload, Map.of());
+        }
+
+        delegate = new ProducerDelegate(factory,
+                List.of(destination),
+                List.of(queue1, queue2),
+                allowDynamicDestinations,
+                Duration.ZERO,
+                elucidationClient,
+                serviceName);
+
+        delegate.producers.putAll(newHashMap(
+            identifier, testProducer,
+            queue1, queue1Producer,
+            queue2, queue2Producer
+        ));
+
+        delegate.produceToDestinationAndAllEventsQueue(destination, payload);
+
+        verify(queue1Producer).produce(payload, Map.of());
+        verify(queue1Producer).isDefaultProducer();
+
+        verify(queue2Producer).produce(payload, Map.of());
+        verify(queue2Producer).isDefaultProducer();
+
+        if (destinationType == DYNAMIC) {
+            verify(testProducer).produce(payload, destination, Map.of());
+        } else {
+            verify(testProducer).produce(payload, Map.of());
+        }
+
+        verifyNoMoreInteractions(queue1Producer, queue2Producer, testProducer);
+        verifyNoInteractions(allEventsProducer);
+
+        verifyElucidationWasInvokedProperly(destination, payload, queue1, queue2);
+    }
+
+    private Producer newMockDefaultProducer(String payload, String destination) {
+        var producer = mock(Producer.class);
+        when(producer.isDefaultProducer()).thenReturn(true);
+        doNothing().when(producer).produce(payload, destination);
+        return producer;
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException_WhenDestinationInInvalid() {
+        var delegate = newDefaultProducerDelegateNoDynamicDestinations();
+
+        var payload = "Test!";
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> delegate.produceToDestination("queue:invalid", payload))
+                .withMessage("Producer 'queue:invalid' not found!");
+
+        verifyNoInteractions(testProducer, allEventsProducer, elucidationClient);
+    }
+
+    @Test
+    void shouldProduceToAllEvents() {
+        var delegate = newDefaultProducerDelegateNoDynamicDestinations();
+
+        var payload = "Test!";
+
+        delegate.produceToAllEventsQueue(payload);
+
+        verify(allEventsProducer).produce(payload, Map.of());
+
+        verifyNoMoreInteractions(allEventsProducer);
+        verifyNoInteractions(testProducer);
+
+        verifyElucidationWasInvokedProperly("queue:all_events", payload);
+    }
+
+    @Test
+    void shouldProduceBytesMessage_WithInvalidDestination() {
+        var delegate = newDefaultProducerDelegateNoDynamicDestinations();
+
+        var payload = "Test!".getBytes(UTF_8);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> delegate.produceBytesMessage("queue:invalid", payload))
+                .withMessage("Producer 'queue:invalid' not found!");
+
+        verifyNoInteractions(testProducer, allEventsProducer, elucidationClient);
+    }
+
+    @Test
+    void shouldProduceBytesMessage_WithKnownDestination() {
+        var delegate = newDefaultProducerDelegateNoDynamicDestinations();
+
+        var destination = "queue:test";
+        var payload = "Test!".getBytes(UTF_8);
+
+        delegate.produceBytesMessage(destination, payload);
+
+        verify(testProducer).produceBytesMessage(payload);
+        verifyNoMoreInteractions(testProducer);
+        verifyNoInteractions(allEventsProducer);
+
+        verifyElucidationWasInvokedProperly(destination, "BYTES_MESSAGE");
+    }
+
+    private ProducerDelegate newDefaultProducerDelegateNoDynamicDestinations() {
+        return newDefaultProducerDelegateAllowingDynamicDestinations(
+            false,
+            NAMED_DESTINATION, testProducer,
+            ALL_EVENTS_DESTINATION, allEventsProducer
+        );
+    }
+
+    private void verifyElucidationWasInvokedProperly(String destination, String payload, String... defaultDestinations) {
+        var shortName = getShortName(destination);
+
+        verify(elucidationClient).recordNewEvent(createEventId(shortName, payload));
+
+        Arrays.stream(defaultDestinations)
+                .forEach(defaultDestination -> {
+                    var defaultShortName = getShortName(defaultDestination);
+                    verify(elucidationClient).recordNewEvent(createEventId(defaultShortName, payload));
+                });
+
+        verifyNoMoreInteractions(elucidationClient);
+    }
+
+    private static String getShortName(String destination) {
+        return destination.split(":")[1];
+    }
+
+    private static String createEventId(String serviceName, String payload) {
+        return serviceName + "::" + payload;
+    }
+}
