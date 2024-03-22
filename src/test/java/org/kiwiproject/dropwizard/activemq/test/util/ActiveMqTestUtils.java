ffc270b1d28acdec4cb46af34884333423482a5b
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-03-22 15:09:31 -0400

    Add/enhance some test utilities (#38)
    * Add ActiveMqMessages test utility class
    * Add new methods to ActiveMsTestUtils class
    * Add ConsumerMessageListener test helper class
    * Add MockJmsMessage
    * Add test for MockActiveMqConsumer

--- src/test/java/org/kiwiproject/dropwizard/activemq/test/util/ActiveMqTestUtils.java
+++ src/test/java/org/kiwiproject/dropwizard/activemq/test/util/ActiveMqTestUtils.java
@@ -1,10 +1,13 @@
 package org.kiwiproject.dropwizard.activemq.test.util;
 
+import static org.kiwiproject.base.KiwiStrings.f;
+
 import lombok.experimental.UtilityClass;
 
 import org.kiwiproject.dropwizard.activemq.util.UncheckedJMSException;
 
 import javax.jms.Connection;
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
@@ -16,6 +19,14 @@ public class ActiveMqTestUtils {
     private static final String LISTENER_PREFIX = "listener-";
     private static final boolean NOT_TRANSACTED = false;
 
+    /**
+     * Create a non-transacted Session with automatic acknowledgement for the given JMS connection.
+     *
+     * @param connection the JMS connection
+     * @return a new Session
+     * @see Connection#createSession(boolean, int)
+     * @see Session#AUTO_ACKNOWLEDGE
+     */
     public static Session createNonTransactedSession(Connection connection) {
         try {
             return connection.createSession(NOT_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
@@ -24,7 +35,58 @@ public class ActiveMqTestUtils {
         }
     }
 
-    public static MessageProducer createProducerQueue(Session session, String name) {
+    /**
+     * Create a new {@link ConsumerMessageListener} whose consumer name will prefixed with "listener-".
+     *
+     * @param destinationName the ActiveMQ destination name
+     * @return a new instance
+     */
+    public static ConsumerMessageListener createConsumerMessageListener(String destinationName) {
+        return new ConsumerMessageListener(f("{}{}", LISTENER_PREFIX, destinationName));
+    }
+
+    /**
+     * Create a new {@link ConsumerMessageListener} for a JMS {@link Queue} having the given name.
+     *
+     * @param session the JMS session that the consumer listener will be attached to
+     * @param queueName the ActiveMQ queue name
+     * @return a new instance
+     */
+    public static ConsumerMessageListener createQueueConsumerMessageListener(Session session, String queueName) {
+        return createConsumerMessageListener(session, queueName, createQueue(session, queueName));
+    }
+
+    /**
+     * Create a new {@link ConsumerMessageListener} for the given session and destination.
+     *
+     * @param session the JMS session that the consumer listener will be attached to
+     * @param destinationName the name of the JMS destination
+     * @param destination the JMS {@link Destination} for which to create the consumer and listener
+     * @return a new instance
+     */
+    public static ConsumerMessageListener createConsumerMessageListener(Session session,
+                                                                        String destinationName,
+                                                                        Destination destination) {
+
+        var listener = createConsumerMessageListener(destinationName);
+
+        try {
+            session.createConsumer(destination).setMessageListener(listener);
+            return listener;
+        } catch (JMSException e) {
+            throw new UncheckedJMSException(e);
+        }
+    }
+
+    /**
+     * Create a {@link MessageProducer} that can send messages to a {@link Queue} with the given name.
+     *
+     * @param session the JMS session
+     * @param name the name of the JMS Queue
+     * @return a new MessageProducer instance
+     * @throws UncheckedJMSException
+     */
+    public static MessageProducer createQueueProducer(Session session, String name) {
         try {
             return session.createProducer(createQueue(session, name));
         } catch (JMSException e) {
@@ -32,7 +94,15 @@ public class ActiveMqTestUtils {
         }
     }
 
-    private static Queue createQueue(Session session, String name) {
+    /**
+     * Creates a {@link Queue}.
+     *
+     * @param session the JMS session
+     * @param name the name of the JMS Queue
+     * @return a new Queue instance
+     * @throws UncheckedJMSException
+     */
+    public static Queue createQueue(Session session, String name) {
         try {
             return session.createQueue(name);
         } catch (JMSException e) {
---
+++ src/test/java/org/kiwiproject/dropwizard/activemq/test/util/ConsumerMessageListener.java
@@ -0,0 +1,47 @@
+package org.kiwiproject.dropwizard.activemq.test.util;
+
+import static com.google.common.base.Preconditions.checkState;
+import static org.kiwiproject.base.KiwiPreconditions.requireNotBlank;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@Slf4j
+public class ConsumerMessageListener implements MessageListener {
+
+    @Getter
+    private final String consumerName;
+
+    private final AtomicInteger count;
+
+    public ConsumerMessageListener(String consumerName) {
+        this.consumerName = requireNotBlank(consumerName);
+        this.count = new AtomicInteger();
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        checkState(message instanceof TextMessage, "only TextMessage is supported");
+        var textMessage = (TextMessage) message;
+
+        var currentCount = count.incrementAndGet();
+
+        try {
+            LOG.info("Consumer: '{}' received message: '{}', total count: {}",
+                    consumerName, textMessage.getText(), currentCount);
+        } catch (Exception e) {
+            LOG.error("Caught exception in onMessage", e);
+            throw new RuntimeException("An unexpected JMSException occured");
+        }
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+}
